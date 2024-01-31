c46d5382b980f1bc25922ecbcd7f75888849ceb5
Arjun Tanwar <arjunthakur900@gmail.com>
2024-01-31 13:19:30 +0530

    feat: more hooks

--- HookUtils.php
+++ HookUtils.php
@@ -1,9 +1,6 @@
 <?php 
-namespace _29kPayments;
-use WP_Error;
-use WP_REST_Request;
-use WP_User;
-class BasePlugin {
+
+class Hooks {
     public function __construct() {}
     public function addShortCode(string $key, string $path, array $attsArr = []) {
         add_action('init', function () use(&$key, &$path, &$attsArr) {
@@ -29,6 +26,14 @@ class BasePlugin {
         });
     }
 
+    public function addScriptWithServerVariable(string $objectNameForFrontend, array $assocArrayToConvert, string $path, array $dependencies = array(), bool $inFooter = \false, string|bool $version = \false) {
+        add_action( 'wp_enqueue_scripts', function () use($path, $objectNameForFrontend, $assocArrayToConvert, $dependencies, $inFooter, $version) {
+            $key = md5($path);
+            wp_enqueue_script($key, $path, $dependencies, $version, $inFooter);
+            wp_localize_script($key, $objectNameForFrontend, $assocArrayToConvert);
+        });
+    }
+
     public function addRestRoute(string $method, string $route, string $endpoint, callable $callback, callable $permissionCallback = null) {
         if(!$permissionCallback) $permissionCallback = function() { return '__return_true'; };
         add_action('rest_api_init', function () use(&$method, &$callback, $route, &$endpoint, $permissionCallback){
@@ -39,6 +44,12 @@ class BasePlugin {
                 },
                 'permission_callback' => $permissionCallback
             ));
+        });
+    }
+
+    public function beforePageLoad(callable $callback) {
+        add_action('template_redirect', function () use(&$callback) {
+            $callback();
         });
     }
 
@@ -100,7 +111,7 @@ class BasePlugin {
         });
     }
 
-    public function addCustomPostType(string $postTypeNameSingular, string $postTypeNamePlural, string $menuIcon) {
+    public function addCustomPostType(string $postTypeNameSingular, string $postTypeNamePlural, string $menuIcon = 'dashicons-admin-post') {
         add_action( 'init', function () use($postTypeNamePlural, $postTypeNameSingular, $menuIcon) {
             $labels = [
                 'name'               => $postTypeNamePlural,
@@ -126,17 +137,79 @@ class BasePlugin {
                 'show_ui'            => true,
                 'show_in_menu'       => true,
                 'query_var'          => true,
-                'rewrite'            => [ 'slug' => $postTypeNamePlural ],
+                'rewrite'            => [ 'slug' => strtolower($postTypeNamePlural) ],
                 'capability_type'    => 'post',
                 'has_archive'        => true,
                 'hierarchical'       => false,
                 'menu_icon'          => $menuIcon,
                 'menu_position'      => null,
                 'supports'           => [ 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields' ],
-                'show_in_rest'       => true
+                'show_in_rest'       => true,
+                'taxonomy'           => ['post-tag', 'category']
             ];  
-            register_post_type($postTypeNamePlural, $args );
+            register_post_type(strtolower($postTypeNamePlural), $args );
         });
+    }
+    function addThemeSupport(string $feature, array $option = []) {
+        add_action('after_setup_theme', function () use($feature, $option) {
+            add_theme_support($feature, $option);
+        });
+    }
+    function addMenu(string $name) {
+        add_action('after_setup_theme', function () use($name) {
+            register_nav_menus([
+                md5($name) => $name
+            ]);
+        });
+    }
+    function addCategoryOrTagToPost(string $categoryNameSingular, string $categoryNamePlural, array $postTypes, bool $isTag = false) {
+        add_action( 'init', function() use($categoryNamePlural, $categoryNameSingular, $postTypes, $isTag) {
+            $labels = array(
+                'name'              => _x($categoryNamePlural, 'taxonomy general name', 'textdomain'),
+                'singular_name'     => _x($categoryNameSingular, 'taxonomy singular name', 'textdomain'),
+                'search_items'      => __('Search ' . $categoryNamePlural, 'textdomain'),
+                'all_items'         => __('All ' . $categoryNamePlural, 'textdomain'),
+                'parent_item'       => __('Parent ' . $categoryNameSingular, 'textdomain'),
+                'parent_item_colon' => __('Parent ' . $categoryNameSingular . ':', 'textdomain'),
+                'edit_item'         => __('Edit ' . $categoryNameSingular, 'textdomain'),
+                'update_item'       => __('Update ' . $categoryNameSingular, 'textdomain'),
+                'add_new_item'      => __('Add New ' . $categoryNameSingular, 'textdomain'),
+                'new_item_name'     => __('New ' . $categoryNameSingular, 'textdomain'),
+                'menu_name'         => __($categoryNamePlural, 'textdomain'),
+            );
+        
+            $args = [
+                'hierarchical'      => !$isTag,  
+                'labels'            => $labels,
+                'public'            => true,
+                'show_ui'           => true,
+                'show_admin_column' => true,
+                'query_var'         => true,
+                'rewrite'           => array('slug' => strtolower($categoryNamePlural)),
+                'has_archive'       => true,
+                'show_in_rest'      => true,
+            ];
+            register_taxonomy(strtolower($categoryNamePlural), $postTypes, $args);
+        }, 0 );
+    }
+
+    public function setLogoutRedirect(string $url) {
+        add_action('wp_logout', function () use(&$url) {
+            wp_safe_redirect($url);
+            exit;
+        });
+    }
+
+    public function addSidebar(string $name, string $id, string $description = '', string $beforeWidget = '', string $afterWidget = '', string $beforeTitle = '', string $afterTitle = '') {
+        register_sidebar([
+            'name'          => $name,
+            'id'            => $id,
+            'description'   => $description,
+            'before_widget' => $beforeWidget,
+            'after_widget'  => $afterWidget,
+            'before_title'  => $beforeTitle,
+            'after_title'  => $afterTitle,
+        ]);
     }
 }
 ?>
